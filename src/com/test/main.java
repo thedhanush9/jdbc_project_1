29e245bdd724970625fc4d5e5e31ca1aa2d00cf5
thedhanush9 <ydhanush54@gmail.com>
2025-05-06 09:31:58 +0530

    Update main.java

--- src/com/test/main.java
+++ src/com/test/main.java
@@ -1,10 +1,15 @@
 package com.test;
-
+import java.util.*;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		System.out.println("What you want to print for n times type that string");
+		String message=sc.next();
+		System.out.println("How Many Times");
+		int n=sc.nextInt();
+		for(int i=0;i<n;i++){
+			System.out.println(message);
+		}
 	}
 
 }
