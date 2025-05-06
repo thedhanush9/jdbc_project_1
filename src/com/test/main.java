443ee24aac15dd2c497ffbc9b115233e18eaec6a
thedhanush9 <ydhanush54@gmail.com>
2025-05-06 09:43:31 +0530

    Update main.java

--- src/com/test/main.java
+++ src/com/test/main.java
@@ -4,8 +4,8 @@ public class main {
 
 	public static void main(String[] args) {
 		System.out.println("Printing Square");
-		for(int i=0;i<6;i++){
-			for(int j=0;j<6;j++{
+		for(int i = 0 ; i < 6 ; i++){
+			for(int j = 0 ; j < 6 ; j++){
 				if(i==0 || j==0 || i==5 || j==5){
 					System.out.print(" * ");
 				}
